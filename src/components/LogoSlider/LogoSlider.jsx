e1ed54ec81117bbf4b06b89009d0d8277732c800
seifeldin15 <seifeldin02@outlook.com>
2025-02-27 11:11:02 +0200

    Added logo slider

---
+++ src/components/LogoSlider/LogoSlider.jsx
@@ -0,0 +1,42 @@
+import React, { useEffect, useRef } from 'react';
+
+const LogoSlider = () => {
+  const logosRef = useRef(null);
+
+  useEffect(() => {
+    // Clone the logos list to create the infinite scroll effect
+    if (logosRef.current) {
+      const clone = logosRef.current.cloneNode(true);
+      clone.setAttribute('aria-hidden', 'true');
+      logosRef.current.parentNode.appendChild(clone);
+    }
+  }, []);
+
+  return (
+    <div className="logo-slider-container">
+      {/* Logo Carousel animation */}
+      <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-128px),transparent_100%)]">
+        <ul ref={logosRef} className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll">
+          <li>
+            <img src="/logos slider/logo1.png" alt="Facebook" />
+          </li>
+          <li>
+            <img src="/logos slider/logo2.png" alt="Disney" />
+          </li>
+          <li>
+            <img src="/logos slider/logo3.png" alt="Airbnb" />
+          </li>
+          <li>
+            <img src="/logos slider/logo4.png" alt="Apple" />
+          </li>
+          <li>
+            <img src="/logos slider/logo5.png" alt="Spark" />
+          </li>
+        </ul>
+      </div>
+      {/* End: Logo Carousel animation */}
+    </div>
+  );
+};
+
+export default LogoSlider;
