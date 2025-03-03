cdd66937925a1c506a28ff7d96b6b2b1030352d3
seifeldin15 <seifeldin02@outlook.com>
2025-03-03 09:31:31 +0200

    Bronze Page

---
+++ src/pages/Bronze.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ServerCompare from '../components/ServerCompare/ServerCompare'
+function Bronze() {
+  return (
+    <div>
+      <ServerCompare />
+    </div>
+  )
+}
+
+export default Bronze
